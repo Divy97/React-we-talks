83255833186c7501f77276e458d4866e12fc08c5
Divy <divyparekh1810@gmail.com>
2022-06-26 19:32:42 +0530

    almost done with client folder

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,21 @@
-import React,{ useState} from 'react'
+import React from 'react'
 import Login from './Login';
-const App = () => {
+import UseLocalStorage from '../Hooks/UseLocalStorage';
+import Dashboard from './Dashboard';
+import {ContactsProvider} from '../contexts/ContactsProvider';
+import { ConversationsProvider } from '../contexts/ConversationsProvider';
+function App() {
+  const [id, setId] = UseLocalStorage('id');
 
-  const [id, setId] = useState();
+  const dashboard = (
+    <ContactsProvider>
+      <ConversationsProvider id={id}>
+        <Dashboard id={id}/>
+      </ConversationsProvider>
+    </ContactsProvider>
+  )
   return(
-    <>
-      {id}
-      <Login onIdSubmit={setId} />
-    </>
+      id ? dashboard : <Login onIdSubmit={setId} />
   )
 }
 
